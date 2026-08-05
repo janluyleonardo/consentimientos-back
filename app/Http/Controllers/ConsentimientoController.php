45027df13aa03eeb7a71379de971c92aef3c8cc4
janluyleonardo <janluy.moreno@gmail.com>
2026-08-05 11:40:21 -0500

    first commit

---
+++ app/Http/Controllers/ConsentimientoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Consentimiento;
+use App\Http\Requests\StoreConsentimientoRequest;
+use App\Http\Requests\UpdateConsentimientoRequest;
+
+class ConsentimientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreConsentimientoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Consentimiento $consentimiento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Consentimiento $consentimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateConsentimientoRequest $request, Consentimiento $consentimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Consentimiento $consentimiento)
+    {
+        //
+    }
+}
---
+++ database/factories/ConsentimientoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Consentimiento;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Consentimiento>
+ */
+class ConsentimientoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
